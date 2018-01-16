c75037d00681d7ffa78f9938e3e6ab493066042b
Eirik Vale Aase <e.v.aase@gmail.com>
2018-01-16 22:13:03 +0100

    Initial Commit

---
+++ Nic/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Nic
+//
+//  Created by Eirik Vale Aase on 16.01.2018.
+//  Copyright © 2018 Eirik Vale Aase. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
